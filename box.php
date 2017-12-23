f3deeb9c844647f8712f892f9394e9b0e0a964b0
hanjy0228 <sun2013220@163.com>
2017-12-23 15:07:59 +0800

    t

--- box.php
+++ box.php
@@ -5,5 +5,19 @@
  * Date: 2017/12/23
  * Time: 14:50
  */
+include "conn.php";
+$sid=$_COOKIE['sid'];
+$sql="select * from user,sx where user.uid=sx.sid and?/* 'user'.uid=sx.rid and*/ sx.sid='$sid'";
+$query=mysqli_query($link,$sql);
+while($rs=mysqli_fetch_array($query)){
+    $arr[]=$rs;
 
+}
+foreach($arr as $k=>$v){
+    $rid=$v['rid'];
+    $sql="select * from user,sx where 'user'.uid=sx.rid and sx.rid='$rid'";
+    $query=mysqli_query($link,$sql);
+    $rows=mysqli_fetch_array($query);
+    $newarr[]=$rows;
+}
 ?>
